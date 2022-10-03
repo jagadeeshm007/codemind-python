1f6e8416228a8121a6feab13736eff96b77e5104
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-03 13:07:55 +0530

    def prime(a):     if a==0 or a==1:         return 0     for i in range (2,a):         if a%i==0:             return 0     return 1 n=input() k=int(n) r=int(n[::-1]) if prime(k)==1 and prime(r)==1 :     print("circular prime") elif prime(k)!=1:     print("not prime") else:     print("prime but not a circular prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,24 +1,16 @@
-def prime(c):
-    if c<2:
+def prime(a):
+    if a==0 or a==1:
         return 0
-    else:
-        for i in range(2,int(c**0.5)+1):
-            if c%i==0:
-                return 0
-        return 1
-def rev(a):
-    t=k=0
-    while a:
-        t=a%10
-        k=(k*10)+t
-        a=a//10
-    return k
-n=int(input())
-k=rev(n)
-if  prime(n)!=1:
+    for i in range (2,a):
+        if a%i==0:
+            return 0
+    return 1
+n=input()
+k=int(n)
+r=int(n[::-1])
+if prime(k)==1 and prime(r)==1 :
+    print("circular prime")
+elif prime(k)!=1:
     print("not prime")
 else:
-    if prime(n)==1 and prime(k)!=1:
-        print("prime but not a circular prime")
-    elif prime(n)==1 and prime(k)==1:
-        print("circular prime")
+    print("prime but not a circular prime")
