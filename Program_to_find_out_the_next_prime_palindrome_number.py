e1fb1682739f44b1bc1d779f822ad420a63283d6
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-07 11:10:19 +0530

    def prime(a):     if a<2:         return 0     else:         for i in range(2,int(a**0.5)+1):             if a%i==0:                 return 0         return 1 a=int(input()) a=a+1 c=0 while str(a)!=(str(a)[::-1]) or prime(a)!=1:         a+=1 print(a)

--- Program_to_find_out_the_next_prime_palindrome_number.py
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -1,41 +1,15 @@
 def prime(a):
-    c=0
-    if a==1:
-        c=1
-    else:
-        for i in range(2,a):
-            if(a%i)==0:
-                c=1
-                break
-    if c==0:
+    if a<2:
         return 0
     else:
+        for i in range(2,int(a**0.5)+1):
+            if a%i==0:
+                return 0
         return 1
-def rev(a):
-    d=a
-    i=s=0
-    while a:
-        s=a%10
-        i=(i*10)+s
-        a=a//10
-    if i==d:
-        return 0
-    else:
-        return 1
-   
-n=int(input())
-n=n+1
-test=1
-while test:
-    if rev(n)==0:
-        if prime(n)==0:
-            test=0
-            break
-        else:
-            n+=1
-    else:
-        n+=1
-print(n)
-    
-    
+a=int(input())
+a=a+1
+c=0
+while str(a)!=(str(a)[::-1]) or prime(a)!=1:
+        a+=1
+print(a)
     
