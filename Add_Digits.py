097ec8a014e9d6f299e63d7a54588ae86c4facc3
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-07 11:39:25 +0530

    a=input() z=int(a) while z>9:     b=list(map(int,a.strip("")))     if sum(b)>9:         a=str(sum(b))     else:         print(sum(b))         break

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,17 +1,10 @@
-def sum(n):
-    t=s=0
-    while n:
-        t=n%10
-        s=s+t
-        n=n//10
-    return s
-n=int(input())
-c=1
-while c:
-    if sum(n)<10:
-        n=sum(n)
-        c=0
-        break
+a=input()
+z=int(a)
+while z>9:
+    b=list(map(int,a.strip("")))
+    if sum(b)>9:
+        a=str(sum(b))
     else:
-        n=sum(n)
-print(n)
+        print(sum(b))
+        break
+    
