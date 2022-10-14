b60e165857d3e464c3b6aaaeee2f9abe7b05742f
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-14 18:48:41 +0530

    n=int(input()) a=list(map(int,input().split())) z=set(a) k=0 c=0 for i in z:     if a.count(i)==i:
            k+=i
            c+=1
    if c==0:
        print(-1)
    else:
        print("%.2f"%(k/c))

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -1,17 +1,15 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=s=z=0
-for i in range(0,n):
-    c=0
-    for j in range(0,n):
-        if a[i]==a[j]:
-            if i!=j:
-                a[j]=1000
-            c+=1
-    if a[i]==c and a[i]!=1000:
-        s+=c
-        z+=1
-if z>=1:
-    print("{:0.2f}".format(s/z))
+z=set(a)
+k=0
+c=0
+for i in z:
+    if a.count(i)==i:
+        
+        k+=i
+        c+=1
+if c==0:
+    print(-1)
 else:
-    print("-1")
+    print("%.2f"%(k/c))
+    
