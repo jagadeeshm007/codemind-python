82807ba0017082df38479b83653c91cb0728073a
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-14 18:55:45 +0530

    n=int(input()) a=list(map(int,input().split())) m=int(input()) z=[] o=0 for i in a:     if i not in z:         z.append(i) for i in z:     c=0     for j in a:         if i==j:             c+=1     if c==m:         o=1         print(i,end=" ") if o==0:     print(-1)

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,17 +1,19 @@
 n=int(input())
 a=list(map(int,input().split()))
-k=int(input())
-c=s=zap=0
-x=z=a[0]
-for i in range(0,n):
+m=int(input())
+z=[]
+o=0
+for i in a:
+    if i not in z:
+        z.append(i)
+for i in z:
     c=0
-    for j in range(0,n):
-        if a[i]==a[j]:
-            if i!=j:
-                a[j]=1000
+    for j in a:
+        if i==j:
             c+=1
-    if c==k and a[i]!=1000:
-        print(a[i],end=" ")
-        zap+=1 
-if zap<1:
-    print("-1")
+    if c==m:
+        o=1
+        print(i,end=" ")
+if o==0:
+    print(-1)
+    
