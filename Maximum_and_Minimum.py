36ecaa11c63366901cf06659fbf564629a62b4cf
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-14 18:50:34 +0530

    n=int(input()) a=list(map(int,input().split())) z=set(a) k=[] for i in z:     if a.count(i)==i:         k.append(i) if k==[]:     print(-1) else:     print(min(k),max(k))

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,22 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=s=zap=0
-x=z=a[0]
-for i in range(0,n):
-    c=0
-    for j in range(0,n):
-        if a[i]==a[j]:
-            if i!=j:
-                a[j]=1000
-            c+=1
-    if a[i]==c and a[i]!=1000:
-        if z<a[i]:
-            z=a[i]
-        if x>a[i]:
-            x=a[i]
-        zap+=1 
-if zap>=1:      
-    print(x,z)
+z=set(a)
+k=[]
+for i in z:
+    if a.count(i)==i:
+        k.append(i)
+if k==[]:
+    print(-1)
 else:
-    print("-1")
-        
+    print(min(k),max(k))
