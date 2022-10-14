d7f5e39c38ae81c6b7662f56b2dfc68a65cee14d
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-14 18:52:02 +0530

    n=int(input()) a=list(map(int,input().split())) z=[] for i in a:     if i not in z:         z.append(i) k=[] for i in z:     if a.count(i)==i:         k.append(i) if k==[]:     print(-1) else:     print(*k)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,17 +1,14 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=s=zap=0
-x=z=a[0]
-for i in range(0,n):
-    c=0
-    for j in range(0,n):
-        if a[i]==a[j]:
-            if i!=j:
-                a[j]=1000
-            c+=1
-    if a[i]==c and a[i]!=1000:
-        print(a[i],end=" ")
-        zap+=1 
-if zap<1:      
-    print("-1")
-        
+z=[]
+for i in a:
+    if i not in z:
+        z.append(i)
+k=[]
+for i in z:
+    if a.count(i)==i:
+        k.append(i)
+if k==[]:
+    print(-1)
+else:
+    print(*k)
