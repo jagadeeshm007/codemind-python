532b598ec6cc4bfd60c1c9d8a7da830c13480f41
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-14 18:19:40 +0530

    def p(n):     if n<2:         return 0     else:         for i in range(2,n):             if (n%i) == 0:                 return 0     return 1 a=int(input()) n=list(map(int,input().split())) k=int(input()) c=0 for i in n:     if i<=k:         if p(i)==1:             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,17 @@
+def p(n):
+    if n<2:
+        return 0
+    else:
+        for i in range(2,n):
+            if (n%i) == 0:
+                return 0
+    return 1
+a=int(input())
+n=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in n:
+    if i<=k:
+        if p(i)==1:
+            c+=1
+print(c)
