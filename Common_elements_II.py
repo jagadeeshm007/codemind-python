9a39c7f94e16202f652a4c9934c3ef9217f1b121
Jagadeesh M <96563707+jagadeeshm007@users.noreply.github.com>
2022-10-16 17:22:23 +0530

    a,b=map(int,input().split()) x=list(map(int,input().split())) y=list(map(int,input().split())) z=[] for i in (x+y):     if i not in z:         z.append(i) d=[] for i in z:     if (i in x and i not in y) or (i not in x and i in y):         d.append(i) print(*d)

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,9 +1,12 @@
-x,y=map(int,input().split())
-l=list(map(int,input().split()))
-m=list(map(int,input().split()))
-for i in m:
-    l.append(i)
-for i in l:
-    k=l.count(i)
-    if(k==1):
-        print(i,end=" ")
+a,b=map(int,input().split())
+x=list(map(int,input().split()))
+y=list(map(int,input().split()))
+z=[]
+for i in (x+y):
+    if i not in z:
+        z.append(i)
+d=[]
+for i in z:
+    if (i in x and i not in y) or (i not in x and i in y):
+        d.append(i)
+print(*d)
